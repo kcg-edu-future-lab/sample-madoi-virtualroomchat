7b1738dd9873909daec7cd04eafc3170038f50d2
Takao Nakaguchi <takao.nakaguchi@gmail.com>
2025-04-28 14:34:48 +0900

    コード整理

--- src/lib/LocalJsonStorage.ts
+++ src/lib/LocalJsonStorage.ts
@@ -1,28 +1,22 @@
-
 export class LocalJsonStorage<Items extends Record<string, any>>{
     constructor(private keyPrefix: string){
     }
+
     get<K extends keyof Items>(
-        name: K,
-        defaultValue: Items[K] | (()=>Items[K] | undefined)): Items[K];
-    get(name: string, defaultValue: any | (()=>any) | undefined) {
+        name: K, defaultValue: Items[K] | (()=>Items[K]) | undefined): Items[K];
+    get(name: string, defaultValue: any) {
         const key = `${this.keyPrefix}:${name}`;
         let ret = JSON.parse(localStorage.getItem(key) || "null");
-        if(ret !== null){
-            return ret;
-        }
+        if(ret !== null) return ret;
         if(typeof defaultValue === 'undefined') return defaultValue;
-        let v = defaultValue;
-        if(typeof v === 'function'){
-            v = v();
-        }
-        localStorage.setItem(key, JSON.stringify(v));
-        return v;
+        ret = defaultValue;
+        if(typeof ret === 'function') ret = ret();
+        localStorage.setItem(key, JSON.stringify(ret));
+        return ret;
     }
 
     set<K extends keyof Items>(
-        name: K,
-        value: Items[K]): void;
+        name: K, value: Items[K]): void;
     set(name: string, value: any){
         const key = `${this.keyPrefix}:${name}`;
         localStorage.setItem(key, JSON.stringify(value));
